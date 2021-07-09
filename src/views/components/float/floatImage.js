32503063ec946741bf171314e62368b6cfdf3038
Rodrigo Zunarelli <rodrigo.zunarelli@gmail.com>
2021-07-09 14:40:18 -0300

    Incluindo instruções para iniciar o projeto

---
+++ src/views/components/float/floatImage.js
@@ -0,0 +1,11 @@
+import YodaBaby from '../../../assets/images/elements/baby-yoda.jpg';
+
+let floatImage = `
+    <div class="main-left" image--effect"> 
+        <a href="#section-two">
+            <img src="${YodaBaby}" alt="Yoda Baby" width="120px" heigth="auto">
+    </div>
+    
+    `
+
+    export default floatImage;
